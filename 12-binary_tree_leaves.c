ff481d5a7c937b317e313206ba7ba62a46a09326
julianfrancor <julianfrancor@gmail.com>
2020-07-21 19:06:24 -0500

    function that counts the leaves in a binary tree - task 12

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - function that counts the leaves in a binary tree
+ * @tree: pointer to the root node of the tree to count the number of leaves
+ * Return: If tree is NULL, the function must return 0.
+ * A NULL pointer is not a leaf
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaves = 0;
+
+	if (!tree)
+		return (0);
+
+	if (!tree->left && !tree->right)
+		leaves += 1;
+	else
+		leaves = (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+
+	return (leaves);
+}
